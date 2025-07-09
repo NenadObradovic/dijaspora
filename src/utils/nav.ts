0db9f1b1bbbe8f118be4865067bb40da3e2801bb
Danijel Maksimovic <maksimovic.danijel989@gmail.com>
2025-07-09 06:53:42 +0200

    feat: map to current page in language switcher

--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -1,5 +1,6 @@
 import { getRelativeLocaleUrl } from 'astro:i18n'
 import { tObject } from './i18nStatic'
+import astroConfig from '../../astro.config.mjs'
 
 export function getNavLinks(locale: string): { label: string; path: string }[] {
   const t = tObject(locale, 'header_nav')
@@ -21,4 +22,49 @@ export function getNavLinks(locale: string): { label: string; path: string }[] {
     { label: t.handbook as string, path: handbookPath },
     { label: t.form as string, path: formPath },
   ]
+}
+
+/**
+ * Maps current page path to the given locale
+ *
+ * @param path current page path
+ * @param locale language locale
+ * @returns page path for the given locale
+ */
+export function getPagePathForLocale(path: string, locale: string): string {
+  const pagePathElements = path.trim().split('/').filter(Boolean)
+  const currentPath = pagePathElements[pagePathElements.length - 1] //only get the page path or language path for index pages
+
+  const currentPagePath = currentPath === '' ? '/' : currentPath
+  // we shouldn't map index page with language
+  if (
+    currentPagePath === '/' ||
+    (astroConfig.i18n?.locales as unknown as string[]).includes(currentPath)
+  ) {
+    return '/'
+  }
+
+  // have to manually map pages as their names are different for english 🤷
+  if (locale === 'en') {
+    switch (currentPagePath) {
+      case 'prirucnik': {
+        return 'handbook'
+      }
+      case 'glasanje': {
+        return 'voting'
+      }
+    }
+  }
+
+  switch (currentPagePath) {
+    case 'handbook': {
+      return 'prirucnik'
+    }
+
+    case 'voting': {
+      return 'glasanje'
+    }
+  }
+
+  return currentPagePath
 }
